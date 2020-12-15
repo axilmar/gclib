3ef322bab99252e4ead0ca71dbdf452b249788ea
Margaritis, Achillefs (I/EE-331, extern) <extern.achillefs.margaritis@elektrobit.com>
2020-12-15 15:47:31 +0200

    Work in progress.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
-#include "gclib/StackPtr.hpp"
-#include "gclib/GlobalPtr.hpp"
-
-
-using namespace gclib;
-
-
-class Foo
-{
-public:
-};
 
 
 int main()
 {
-    StackPtr<Foo> foo1 = new Foo;
-    GlobalPtr<Foo> foo2 = foo1;
     return 0;
 }
