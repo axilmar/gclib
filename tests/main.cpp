73ef145e16aef38d8969ea57089704bf9cb34d8f
Margaritis, Achillefs (I/EE-331, extern) <extern.achillefs.margaritis@elektrobit.com>
2020-12-22 10:52:37 +0200

    Redesign.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 #include <chrono>
-#include <memory>
-#include "gclib/make.hpp"
-#include "gclib/MemoryResource.hpp"
-
-
-using namespace gclib;
 
 
 template <class F> double timeFunction(F&& func) {
@@ -16,49 +10,7 @@ template <class F> double timeFunction(F&& func) {
 }
 
 
-class Foo {
-public:
-};
-
-
-template <class T> class MemoryResourceAllocator {
-public:
-    MemoryResource mr;
-
-    template <class U> struct rebind {
-        typedef MemoryResourceAllocator<U> other;
-    };
-
-    void* allocate(size_t n) {
-
-    }
-};
-
-
-void test0() {
-    MemoryResourceAllocator<Foo> alloc;
-    double dur = timeFunction([&]() {
-        for (std::size_t i = 0; i < 5'000'000; ++i) {
-            std::shared_ptr<Foo> foo1{ std::allocate_shared<Foo>(alloc) };
-        }
-    });
-    std::cout << dur << " seconds\n";
-}
-
-
-void test1() {
-    double dur = timeFunction([]() {
-        for (std::size_t i = 0; i < 5'000'000; ++i) {
-            VoidPtr foo1 = make<Foo>();
-            dispose(foo1);
-        }
-    });
-    std::cout << dur << " seconds\n";
-}
-
-
 int main() {
-    test0();
     system("pause");
     return 0;
 }
