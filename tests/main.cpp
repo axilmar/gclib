1a9e2463aafd44f4119af9f2f32410fee9672504
Margaritis, Achillefs (I/EE-331, extern) <extern.achillefs.margaritis@elektrobit.com>
2020-12-27 12:06:21 +0200

    Tests in progress.

--- tests/main.cpp
+++ tests/main.cpp
@@ -24,20 +24,21 @@ template <class F> void doTest(const std::string& name, F&& func) {
     const std::size_t prevErrorCount = errorCount;
     double dur = timeFunction(std::forward<F>(func));
     if (errorCount == prevErrorCount) {
-        std::cout << ": OK. Duration: " << dur << " seconds\n";
+        std::cout << ": OK. Duration: " << dur << " seconds.\n";
     }
     else {
         std::cout << "Errors: " << (errorCount - prevErrorCount);
         std::cout << ". Duration: " << dur << " seconds.\n\n";
     }
+    GC::collect();
 }
 
 
 std::atomic<int> count{ 0 };
 
 
-void waitCollection() {
-    while (count.load(std::memory_order_acquire) > 0) {
+void waitCollection(int targetCount) {
+    while (count.load(std::memory_order_acquire) > targetCount) {
         std::this_thread::yield();
     }
 }
@@ -72,9 +73,80 @@ void test1() {
     doTest("1 object, 0 collectables", []() {
         GCPtr<Foo> foo1 = gcnew<Foo>();
         std::size_t prevAllocSize = GC::getAllocSize();
+        int prevCount = count;
         const std::size_t allocSize = GC::collect();
         check(allocSize == prevAllocSize, "No data should have been collected");
-        check(count == 1, "Object count not 1");
+        check(count == prevCount, "No object should have been collected");
+    });
+}
+
+
+void test2() {
+    doTest("1 object, 1 collectable", []() {
+        GCPtr<Foo> foo1 = gcnew<Foo>();
+        std::size_t prevAllocSize = GC::getAllocSize();
+        foo1 = nullptr;
+        int prevCount = count;
+        const std::size_t allocSize = GC::collect();
+        waitCollection(0);
+        check(allocSize <= prevAllocSize, "Data should have been collected");
+        check(prevCount - count == 1, "1 object should have been collected");
+    });
+}
+
+
+void test3() {
+    doTest("2 objects, 0 collectables, 0 cycles", []() {
+        GCPtr<Foo> foo1 = gcnew<Foo>();
+        foo1->other = gcnew<Foo>();
+        std::size_t prevAllocSize = GC::getAllocSize();
+        int prevCount = count;
+        const std::size_t allocSize = GC::collect();
+        check(allocSize == prevAllocSize, "No data should have been collected");
+        check(count == prevCount, "No object should have been collected");
+    });
+}
+
+
+void test4() {
+    doTest("2 objects, 0 collectables, 1 cycle", []() {
+        GCPtr<Foo> foo1 = gcnew<Foo>();
+        foo1->other = gcnew<Foo>();
+        foo1->other->other = foo1;
+        std::size_t prevAllocSize = GC::getAllocSize();
+        int prevCount = count;
+        const std::size_t allocSize = GC::collect();
+        check(allocSize == prevAllocSize, "No data should have been collected");
+        check(count == prevCount, "No object should have been collected");
+    });
+}
+
+
+void test5() {
+    doTest("2 objects, 2 collectables, 0 cycles", []() {
+        GCPtr<Foo> foo1 = gcnew<Foo>();
+        foo1->other = gcnew<Foo>();
+        std::size_t prevAllocSize = GC::getAllocSize();
+        foo1 = nullptr;
+        int prevCount = count;
+        const std::size_t allocSize = GC::collect();
+        check(allocSize < prevAllocSize, "Data should have been collected");
+        check(prevCount - count == 2, "2 objects should have been collected");
+    });
+}
+
+
+void test6() {
+    doTest("2 objects, 2 collectables, 1 cycle", []() {
+        GCPtr<Foo> foo1 = gcnew<Foo>();
+        foo1->other = gcnew<Foo>();
+        foo1->other->other = foo1;
+        std::size_t prevAllocSize = GC::getAllocSize();
+        foo1 = nullptr;
+        int prevCount = count;
+        const std::size_t allocSize = GC::collect();
+        check(allocSize < prevAllocSize, "Data should have been collected");
+        check(prevCount - count == 2, "2 objects should have been collected");
     });
 }
 
@@ -82,6 +154,14 @@ void test1() {
 int main() {
     std::cout << std::fixed;
     test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    if (errorCount > 0) {
+        std::cout << "Errors: " << errorCount << std::endl;
+    }
     system("pause");
-    return 0;
+    return errorCount;
 }
